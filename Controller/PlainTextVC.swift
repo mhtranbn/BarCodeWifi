9e22b078d228d9d9e1646c4ab9bfa6155055c618
mhtran <tranmanhhoang@gmail.com>
2016-04-19 15:20:14 +0700

    fuck gitlab

---
+++ Controller/PlainTextVC.swift
@@ -0,0 +1,20 @@
+//
+//  PlainTextVC.swift
+//  BarCodeWifi
+//
+//  Created by mhtran on 4/19/16.
+//  Copyright © 2016 mhtran. All rights reserved.
+//
+
+import UIKit
+
+class PlainTextVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
